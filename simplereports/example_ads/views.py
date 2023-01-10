af554db279312573d31156c3c3678c078f85ddef
Кривошеев Никита <sharkbreeder@yandex.ru>
2023-01-10 17:59:23 +0300

    applied

--- simplereports/example_ads/views.py
+++ simplereports/example_ads/views.py
@@ -15,3 +15,18 @@ def index3(request):
     template = 'example/index3.html'
     context = {}
     return render(request, template, context)
+
+def about(request):
+    template = 'example_ads/about.html'
+    context = {}
+    return render(request, template, context)
+
+def constructor(request):
+    template = 'example_ads/constructor.html'
+    context = {}
+    return render(request, template, context)
+
+def report(request):
+    template = 'example_ads/report.html'
+    context = {}
+    return render(request, template, context)
