1bbfaedc611d165b4c67eb94aa10fc2b246ee1b2
Кривошеев Никита <sharkbreeder@yandex.ru>
2023-01-10 16:53:28 +0300

    damn

--- simplereports/example_ads/urls.py
+++ simplereports/example_ads/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
+
 
 app_name = 'example_ads'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('report/', views.index3, name='index3'),
     path('', views.index2, name='index2'),
     path('demo/', views.index, name='index'),
+    path('api/', include('example_api.urls', namespace='api')),
 ]
