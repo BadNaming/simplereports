9764f3129a2d4052ff645ad5ad92f781fde59ebc
Кривошеев Никита <sharkbreeder@yandex.ru>
2023-01-25 18:00:02 +0300

    try take token

--- simplereports/api/views.py
+++ simplereports/api/views.py
@@ -1,9 +1,11 @@
+import requests
+import json
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 
-from reports.models import Cabinet, Campaign, METRICS, ReportTask
+from reports.models import Cabinet, Campaign, METRICS, ReportTask, TemporaryData
 from .mixins import ForbiddenMethodsMixin
 from .serializers import ReportInfoSerializer, ReportTaskSerializer
 
@@ -55,3 +57,28 @@ def get_info(request):
     return Response(
         serializer.data, status=status.HTTP_200_OK
     )
+
+
+@api_view(['GET', 'POST'])
+def callback(request):
+    get_params = request.query_params
+    code = get_params.get('code')
+    token = get_params.get('token')
+    if code and not token:
+        tem_data = TemporaryData.objects.create(code=code)
+        params = {
+            'client_id': '51521857',
+            'client_secret': 'J5dnCVJIR4J7691XcGi5',
+            'redirect_uri': 'https://test-junglegym.online/api/v1/vk/',
+            'code': code,
+            'scope': 'ads'
+
+        }
+        response = requests.get('https://oauth.vk.com/access_token', params=params)
+        current_token = json.loads(response.text)
+        tem_data.response = current_token
+        tem_data.save()
+        token = current_token['access_token']
+        tem_data.token = token
+        tem_data.save()
+    return Response(status=status.HTTP_200_OK)
