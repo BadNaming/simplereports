078be024bb3fb809f64bcc62b3bcc108c614acc3
Кривошеев Никита <sharkbreeder@yandex.ru>
2023-01-25 18:09:31 +0300

    fix name/manage

--- simplereports/api/urls.py
+++ simplereports/api/urls.py
@@ -9,6 +9,6 @@ router_v1.register(r'report_task', ReportTaskViewset)
 
 urlpatterns = [
     path('v1/info/', get_info, name='get_info'),
-    path('v1/vk/', callback, manage='callback'),
+    path('v1/vk/', callback, name='callback'),
     path('v1/', include(router_v1.urls)),
 ]
