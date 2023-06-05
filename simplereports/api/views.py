3912392dcc4de82284d443a9aea80d9d2277b168
Max Tikhonov <i@t1sha.ru>
2023-06-05 21:48:07 +0300

    report_v4

--- simplereports/api/views.py
+++ simplereports/api/views.py
@@ -1,16 +1,14 @@
 import requests
+from requests.exceptions import HTTPError
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_404_NOT_FOUND
-from rest_framework.exceptions import NotFound
-
+from rest_framework.status import HTTP_200_OK
 
 from exceptions import DataNotReceivedException, InvalidDataException
 from .serializers import ReportSerializer
 from .vk_config import (
     GENERAL_URL,
-    GETUSER,
     GETPLANS,
     REQUEST_HEADERS,
     METRICS_VK
@@ -22,8 +20,8 @@ def get_report(request):
     url = GENERAL_URL + GETPLANS
     data = {'campaigns': []}
     if requests.get(url,
-                    headers=REQUEST_HEADERS).status_code == HTTP_404_NOT_FOUND:
-        raise NotFound('Страница не найдена')
+                    headers=REQUEST_HEADERS).status_code != HTTP_200_OK:
+        raise HTTPError('Ответ не получен')
 
     plans = requests.get(url, headers=REQUEST_HEADERS).json()
     for campaign in plans.get('items'):
@@ -41,4 +39,4 @@ def get_report(request):
             raise InvalidDataException(serializer.errors)
         raise DataNotReceivedException()
 
-    return Response(serializer.data)
+    return Response(serializer.data, status=HTTP_200_OK)
